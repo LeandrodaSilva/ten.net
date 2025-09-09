642c1b1e58bcf1d2c1884e3a2cbbd3e779e54d25
LEANDRO DA SILVA <leandroda.silva@sebsa.com.br>
2025-09-08 21:49:51 -0300

    feat: Permite criar uma página server side rendering

---
+++ app/hello/route.ts
@@ -0,0 +1,9 @@
+export function GET(_req: Request): Response {
+  return new Response(JSON.stringify({
+    name: "Leandro"
+  }), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
--- src/ten.ts
+++ src/ten.ts
@@ -1,34 +1,34 @@
-import {walk} from "@std/fs/walk";
-import {transpileFile} from "./trans.ts";
+import { walk } from "@std/fs/walk";
+import { transpileFile } from "./trans.ts";
 
 export type RouteInfo = {
   route: string;
   regex: RegExp;
   hasPage: boolean;
+  transpiledCode: string;
 };
 
 interface DefaultContext<P> {
   params?: P;
 }
 
-const TRANSPILED_CODE: any = {};
-
-for await (const entry of walk("./app", { includeDirs: false, exts: [".ts"] })) {
-  const sourcePath = entry.path;
-  const outPath = sourcePath.replace(/\.ts$/, ".js");
-  try {
-    console.log("Transpilando", sourcePath, "->", outPath);
-    TRANSPILED_CODE[`@${sourcePath}`] = await transpileFile(sourcePath);
-  } catch (e) {
-    console.error(`Erro ao transpilar ${sourcePath}:`, e);
-  }
-}
+// const TRANSPILED_CODE: any = {};
+//
+// for await (const entry of walk("./app", { includeDirs: false, exts: [".ts"] })) {
+//   const sourcePath = entry.path;
+//   const outPath = sourcePath.replace(/\.ts$/, ".js");
+//   try {
+//     console.log("Transpilando", sourcePath, "->", outPath);
+//     TRANSPILED_CODE[`@${sourcePath}`] = await transpileFile(sourcePath);
+//   } catch (e) {
+//     console.error(`Erro ao transpilar ${sourcePath}:`, e);
+//   }
+// }
 
 export class Ten<C extends DefaultContext<any>> {
   private readonly _appPath = "./app";
   private readonly _routeFileName = "route.ts";
   private readonly _routes: RouteInfo[] = [];
-  private _context: (req: Request) => Partial<C> = (req: Request): {} => ({})
 
   static net<C extends DefaultContext<any>>(): Ten<C> {
     return new Ten<C>();
@@ -58,17 +58,21 @@ export class Ten<C extends DefaultContext<any>> {
       if (!entry.isDirectory) continue;
 
       let hasPage = false;
+      let hasRoute = false;
 
       try {
         await Deno.stat(`${entry.path}/${this._routeFileName}`);
+        hasRoute = true;
       } catch {
-        try {
-          await Deno.stat(`${entry.path}/page.html`);
-          hasPage = true;
-        } catch {
-          continue;
-        }
       }
+
+      try {
+        await Deno.stat(`${entry.path}/page.html`);
+        hasPage = true;
+      } catch {
+      }
+
+      if (!hasRoute && !hasPage) continue;
 
       const rel = entry.path
         .replace(/^[.\/]*app/, "") // remove prefixos como './app' ou 'app'
@@ -76,18 +80,31 @@ export class Ten<C extends DefaultContext<any>> {
 
       const route = rel.length ? rel : "/";
 
+      const sourcePath = `${entry.path}/route.ts`;
+      const outPath = sourcePath.replace(/\.ts$/, ".js");
+      let transpiledCode = "";
+      try {
+        console.log("Transpilando", sourcePath, "->", outPath);
+        transpiledCode = await transpileFile(sourcePath);
+      } catch (e) {
+        console.error(`Erro ao transpilar ${sourcePath}:`, e);
+      }
 
       routes.push({
         route,
         regex: this._toRegex(route),
         hasPage,
+        transpiledCode,
       });
     }
 
     return routes;
   }
 
-  private _pathNamedParams(path: string, route: string): Record<string, string> {
+  private _pathNamedParams(
+    path: string,
+    route: string,
+  ): Record<string, string> {
     const params: Record<string, string> = {};
     const pathSegments = path.split("/").filter(Boolean);
     const routeSegments = route.split("/").filter(Boolean);
@@ -99,11 +116,6 @@ export class Ten<C extends DefaultContext<any>> {
       }
     });
     return params;
-  }
-
-  public setContext(context: (req: Request) => Partial<C>): Ten<C> {
-    this._context = context;
-    return this;
   }
 
   private _findOrderedLayouts(route: string): string[] {
@@ -134,66 +146,91 @@ export class Ten<C extends DefaultContext<any>> {
     }
   }
 
-  public async start() {
-    this._routes.push(...await this._buildRoutes());
-    console.info("Routes:", this._routes.map(r => r.route));
+  private async _handleRequest(req: Request): Promise<Response> {
+    const url = new URL(req.url);
+    const path = url.pathname;
+    const method = req.method.toUpperCase();
 
-    Deno.serve(async (req: Request): Promise<Response> => {
-      const url = new URL(req.url);
-      const path = url.pathname;
-      const method = req.method.toUpperCase();
+    const match = this._routes.find((r) => r.regex.test(path));
+    if (!match) return new Response("Not found", { status: 404 });
 
-      const match = this._routes.find((r) => r.regex.test(path));
-      if (!match) return new Response("Not found", { status: 404 });
+    try {
+      console.info(
+        "Module called path:",
+        `@app${match.route}/${this._routeFileName}`,
+      );
+      const module = await import(
+        "data:application/javascript," +
+          encodeURIComponent(match.transpiledCode)
+      ) as unknown as any;
+      console.info("Module called:", module);
+      const fn = module[method] as
+        | ((req: Request, ctx: C) => Response | Promise<Response>)
+        | undefined;
+      const params = this._pathNamedParams(path, match.route);
+      if (typeof fn === "function" && !match.hasPage) {
+        return fn(req, {
+          params,
+        } as C);
+      }
 
-      try {
-        console.info("Module called path:", `@app${match.route}/${this._routeFileName}`);
-        const module = await import("data:application/javascript," + encodeURIComponent(TRANSPILED_CODE[`@app${match.route}/route.ts`])) as unknown as any;
-        console.info("Module called:", module);
-        const fn = module[method] as
-          | ((req: Request, ctx: C) => Response | Promise<Response>)
-          | undefined;
-        const params = this._pathNamedParams(path, match.route);
-        if (typeof fn === "function" && !match.hasPage) {
-          return fn(req, {
-            params,
-            ...this._context(req),
-          } as C);
-        }
-        if (Object.keys(module).length === 0) {
-          throw new Error("Module is empty");
-        }
-        return new Response("Method not implemented", { status: 501 });
-      } catch(e) {
-        if (match.hasPage && method === "GET") {
-          try {
-            const pageModule = Deno.readTextFileSync(
-              `${this._appPath}${match.route}/page.html`,
-            );
-            const layouts = this._findOrderedLayouts(match.route);
-            const documentLayout = this._findDocumentLayoutRoot();
-            let fullContent = documentLayout.replace("{{content}}", pageModule);
-            if (layouts) {
-              for (let i = layouts.length - 1; i >= 0; i--) {
-                const layoutContent = Deno.readTextFileSync(layouts[i]);
-                fullContent = layoutContent.replace("{{content}}", fullContent);
-              }
-              return new Response(fullContent, {
-                status: 200,
-                headers: { "Content-Type": "text/html" },
-              });
+      if (Object.keys(module).length === 0 && !match.hasPage) {
+        throw new Error("Module is empty");
+      }
+
+      if (match.hasPage && method === "GET") {
+        try {
+          const pageModule = Deno.readTextFileSync(
+            `${this._appPath}${match.route}/page.html`,
+          );
+          const layouts = this._findOrderedLayouts(match.route);
+          const documentLayout = this._findDocumentLayoutRoot();
+          let fullContent = documentLayout.replace("{{content}}", pageModule);
+          if (layouts) {
+            for (let i = layouts.length - 1; i >= 0; i--) {
+              const layoutContent = Deno.readTextFileSync(layouts[i]);
+              fullContent = layoutContent.replace("{{content}}", fullContent);
             }
-            return new Response(pageModule, {
+
+            try {
+              const routeResponse = await fn(req, {
+                params,
+              } as C) as Response;
+
+              if (routeResponse) {
+                const body = await routeResponse.json();
+                const keys = Object.keys(body);
+
+                keys.forEach((key) => {
+                  fullContent = String(fullContent).replace(`{{${key}}}`, body[key])
+                })
+              }
+            } catch (e) {
+              console.error(e);
+            }
+
+            return new Response(fullContent, {
               status: 200,
               headers: { "Content-Type": "text/html" },
             });
           }
-          catch {
-            return new Response("Not found", { status: 404 });
-          }
+          return new Response(pageModule, {
+            status: 200,
+            headers: { "Content-Type": "text/html" },
+          });
+        } catch {
+          return new Response("Not found", { status: 404 });
         }
-        return new Response("Not found", { status: 404 });
       }
-    });
+      return new Response("Not found", { status: 404 });
+    } catch (e) {
+      return new Response("Method not implemented", { status: 500 });
+    }
+  }
+
+  public async start() {
+    this._routes.push(...await this._buildRoutes());
+    console.info("Routes:", this._routes.map((r) => r.route));
+    Deno.serve(this._handleRequest.bind(this));
   }
 }
