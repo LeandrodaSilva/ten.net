d8721b8b985ca53f9abfbe8c32777644fa4caac2
Leandro da Silva <leandroda.silva@seb.com.br>
2025-09-13 17:35:59 -0300

    feat: Add paramsEngine and ProgressIndicator classes for enhanced routing and progress tracking

---
+++ src/paramsEngine.ts
@@ -0,0 +1,9 @@
+import {pathNamedParams} from "./utils/pathNamedParams.ts";
+import {Route} from "./models/Route.ts";
+
+export function paramsEngine(path: string, route: Route) {
+	const rawParams = pathNamedParams(path, route.path);
+	return Object.fromEntries(
+		Object.entries(rawParams).filter(([_, value]) => value !== undefined),
+	) as Record<string, string>;
+}
